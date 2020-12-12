0bff896d509344c46a31f952ebd7812de14141f3
estherHeAp <72880303+estherHeAp@users.noreply.github.com>
2020-12-12 17:51:08 +0100

    Cambio configuración DB

--- core/config.php
+++ core/config.php
@@ -11,9 +11,9 @@ define('A_DEF', 'index');
 /**
  * Parámetros de configuración de la BD
  */
-define('HOST', 'localhost');
-define('DBNAME', 'medicontrol');
-define('USER', 'root');
-define('PASS', '');
+define('HOST', 'f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+define('DBNAME', 'qntt7dgxqvdax2qh');
+define('USER', 'y8thkwwxfn3jnaqg');
+define('PASS', 'vfnu7c1ebri76pe7');
 
 define('DNS', 'mysql:host='.HOST.';dbname='.DBNAME);
